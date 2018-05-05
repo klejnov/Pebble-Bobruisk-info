819f034034219605d6b42f9b9192c5ff2451ef64
Олег Клейнов <32246783+klejnov@users.noreply.github.com>
2018-05-06 01:18:23 +0300

    Курсы для Нац. банка
    Добавлены курсы для Нац. банка (сегодня/завтра)

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,6 @@ CurrentTime.setMinutes(CurrentTime.getMinutes() + 5);
 console.log(CurrentTime);
 
 
-
 var appGlanceSlices = [{
     "layout": {
         "subtitleTemplateString": "Pogoda i kursi valut"
@@ -24,7 +23,6 @@ function appGlanceSuccess(appGlanceSlices, appGlanceReloadResult) {
 function appGlanceFailure(appGlanceSlices, appGlanceReloadResult) {
     console.log('FAILURE!');
 }
-
 
 
 // Loading Window Weather
@@ -124,8 +122,19 @@ var loadingTitleCurrency_nbrb = new UI.Text({
     backgroundColor: 'red'
 });
 
-var loadingValueCurrency_nbrb = new UI.Text({
+var loadingTitleCurrency_nbrbToday = new UI.Text({
     position: new Vector2(0, 145),
+    size: new Vector2(144, 25),
+    text: '',
+    font: 'GOTHIC_18',
+    color: 'white',
+    textOverflow: 'wrap',
+    textAlign: 'center',
+    backgroundColor: 'red'
+});
+
+var loadingValueCurrency_nbrb = new UI.Text({
+    position: new Vector2(0, 170),
     size: new Vector2(144, 75),
     text: '',
     font: 'GOTHIC_24_BOLD',
@@ -134,6 +143,47 @@ var loadingValueCurrency_nbrb = new UI.Text({
     textAlign: 'center',
     backgroundColor: 'green'
 });
+
+// Loading Window Home
+var WindowHome = new UI.Window({
+    status: true,
+    scrollable: true
+});
+
+var loadingBgHome = new UI.Rect({
+    position: new Vector2(0, 0),
+    size: new Vector2(144, 168), // full width and height of pebble window
+    backgroundColor: 'clear', // in-built colours
+});
+
+var icoWeatherHome = new UI.Image({
+    position: new Vector2(0, 8),
+    size: new Vector2(144, 50),
+    image: ''
+});
+
+var loadingTemperatureHome = new UI.Text({
+    position: new Vector2(0, 60),
+    size: new Vector2(144, 35),
+    text: '',
+    font: 'GOTHIC_28_BOLD',
+    color: 'white',
+    textOverflow: 'wrap',
+    textAlign: 'center',
+    backgroundColor: 'clear'
+});
+
+var loadingInfoHome = new UI.Text({
+    position: new Vector2(0, 90),
+    size: new Vector2(144, 70),
+    text: '',
+    font: 'GOTHIC_18', // in-built font
+    color: 'white',
+    textOverflow: 'wrap',
+    textAlign: 'center',
+    backgroundColor: 'clear'
+});
+
 
 var menu = new UI.Menu({
     backgroundColor: 'black',
@@ -153,9 +203,9 @@ var menu = new UI.Menu({
   }]
 });
 
-
 menu.show();
 updateWeather();
+updateCurrency();
 
 
 menu.on('select', function (e) {
@@ -166,6 +216,7 @@ menu.on('select', function (e) {
     if (e.itemIndex === 0) {
 
         updateWeather();
+
         // Add background and text elements to the window
         WindowWeather.add(loadingBg);
         WindowWeather.add(icoWeather);
@@ -177,7 +228,9 @@ menu.on('select', function (e) {
 
         // main.show();
     } else if (e.itemIndex === 1) {
+
         updateCurrency();
+        updateCurrencyNbrb();
 
         WindowCurrency.add(loadingCurrencyBg);
         WindowCurrency.add(loadingTitleCurrency);
@@ -185,6 +238,7 @@ menu.on('select', function (e) {
         WindowCurrency.add(loadingUpdateCurrency);
 
         WindowCurrency.add(loadingTitleCurrency_nbrb);
+        WindowCurrency.add(loadingTitleCurrency_nbrbToday);
         WindowCurrency.add(loadingValueCurrency_nbrb);
 
         console.log('Открыто меню курсов валют');
@@ -209,11 +263,11 @@ function updateWeather() {
             url: 'http://klejnov.bobr.by:81/pebble_weather.php?weather=show',
             type: 'json'
         },
-        function (data) {
+        function (data, ddd) {
 
             loadingTemperature.text(data.info);
 
-        //Заменим подпись меню при старте на текущую температуру
+            //Заменим подпись меню при старте на текущую температуру
             var section = {
                 title: 'Меню',
                 items: [{
@@ -223,7 +277,7 @@ function updateWeather() {
                         }, {
                     title: 'Курсы валют',
                     icon: 'images/exchange_rates.png'
-                            }]
+    }]
             };
             menu.section(0, section);
 
@@ -240,7 +294,7 @@ function updateWeather() {
             type: 'json'
         },
         function (data) {
-            loadingInfo.text(data.currentData.summary + '\nВлажность: ' + data.currentData.humidity + ' %' + '\nТочка росы: ' + data.currentData.dewPoint + ' °C' + '\nВетер: ' + data.currentData.windSpeed + ' м/с' + '\nПорывы ветра: ' + data.currentData.windGust + ' м/с' + '\nДавление: ' + data.currentData.pressure + ' мм рт. ст.' + '\nУФ индекс: ' + data.currentData.uvIndex + '' + '\nОзоновый слой: ' + data.currentData.ozone * 0.01 + ' мм.');
+            loadingInfo.text(data.currentData.summary + '\nВлажность: ' + data.currentData.humidity + ' %' + '\nТочка росы: ' + data.currentData.dewPoint + ' °C' + '\nВетер: ' + data.currentData.windSpeed + ' м/с' + '\nПорывы ветра: ' + data.currentData.windGust + ' м/с' + '\nДавление: ' + data.currentData.pressure + ' мм рт. ст.' + '\nУФ индекс: ' + data.currentData.uvIndex + '' + '\nОзоновый слой: ' + (data.currentData.ozone * 0.01).toFixed(2) + ' мм');
 
             if (data.currentData.icon == 'clear-day') {
                 icoWeather.image('images/ic_weather_clear_day.png');
@@ -273,9 +327,47 @@ function updateWeather() {
     );
 }
 
-function updateWeatherHome() {
-    console.log('Длинное нажатеи');
+function WeatherHomeShow() {
+
+    updateWeatherHome();
+
+    WindowHome.add(loadingBgHome);
+    WindowHome.add(icoWeatherHome);
+    WindowHome.add(loadingTemperatureHome);
+    WindowHome.add(loadingInfoHome);
+
+    // Show the window
+    WindowHome.show();
+    console.log('Длинное нажатие 1');
 }
+
+function updateWeatherHome() {
+
+    loadingInfoHome.text('Данные');
+    icoWeatherHome.image('images/house-icon.png');
+    loadingTemperatureHome.text('Обновление');
+
+    // Получаем json из API
+    ajax({
+            url: 'https://api.thingspeak.com/channels/437045/feeds.json?results=2',
+            type: 'json'
+        },
+        function (data) {
+
+            loadingTemperatureHome.text(data.feeds[1].field5 + ' °C');
+            loadingInfoHome.text('Влажн. дом: ' + data.feeds[1].field2 + ' %' + '\nВлажн. улица: ' + data.feeds[1].field6 + ' %' + '\nДавление:\n' + data.feeds[1].field4 + ' мм рт. ст.' + '\nТемпература в корпусе: ' + data.feeds[1].field3 + ' °C');
+            console.log('WindowHome success: ' + data.feeds[1].field5);
+
+        },
+        function (error) {
+            // Failure!
+            console.log('WindowHome Failed data: ' + JSON.stringify(error));
+        }
+    );
+
+    console.log('Длинное нажатие 2');
+}
+
 
 function updateCurrency() {
 
@@ -300,7 +392,21 @@ function updateCurrency() {
         }
     );
 
-  
+}
+
+var usd_nbrb = '';
+var eur_nbrb = '';
+var rub_nbrb = '';
+
+var usd_nbrb_next = '';
+var eur_nbrb_next = '';
+var rub_nbrb_next = '';
+
+var flagNbrb = false;
+var flagNbrbNext = false;
+
+function updateCurrencyNbrb() {
+
     // Получаем json из API
     ajax({
             url: 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0',
@@ -308,61 +414,80 @@ function updateCurrency() {
         },
         function (data) {
 
-            var usd_nbrb = data[4].Cur_OfficialRate;
-            var eur_nbrb = data[5].Cur_OfficialRate;
-            var rub_nbrb = data[16].Cur_OfficialRate;
-
-            loadingValueCurrency_nbrb.text('$ ' + usd_nbrb + '\n' + ' € ' + eur_nbrb + ' \n ' + ' P ' + rub_nbrb);
+            usd_nbrb = data[4].Cur_OfficialRate;
+            eur_nbrb = data[5].Cur_OfficialRate;
+            rub_nbrb = data[16].Cur_OfficialRate;
 
             console.log('Нац.банк USD сегодня: ' + data[4].Cur_OfficialRate + 'дата: ' + data[4].Date);
+
+            flagNbrb = true;
+            showNbrb();
         },
         function (error) {
             // Failure!
             console.log('Failed data: ' + JSON.stringify(error));
         }
     );
-  
-      // Сгенерируем завтрашнюю дату
-  var nextDayNbrb = new Date();
-  nextDayNbrb.setDate(nextDayNbrb.getDate() - 2);
-  nextDayNbrb = (nextDayNbrb).toJSON();
-  console.log('Дата: ' + nextDayNbrb);
-  
-  
-  // Получаем json из API
+
+    // Сгенерируем завтрашнюю дату
+    var nextDayNbrb = new Date();
+    nextDayNbrb.setDate(nextDayNbrb.getDate() + 1);
+    nextDayNbrb = (nextDayNbrb).toJSON();
+    console.log('Дата: ' + nextDayNbrb);
+
+
+    // Получаем json из API
     ajax({
             url: 'http://www.nbrb.by/API/ExRates/Rates?onDate=' + nextDayNbrb + '&Periodicity=0',
             type: 'json'
         },
         function (data) {
-          
-            var usd_nbrb_next = data[4].Cur_OfficialRate;
-            var eur_nbrb_next = data[5].Cur_OfficialRate;
-            var rub_nbrb_next = data[16].Cur_OfficialRate;
-          
-            loadingValueCurrency_nbrb.text('$ ' + usd_nbrb_next + '\n' + ' € ' + eur_nbrb_next + ' \n ' + ' P ' + rub_nbrb_next);
 
-            console.log('Нац.банк USD завтра: ' + data[4].Cur_OfficialRate + 'дата: ' + data[4].Date);
+            if (data[4] !== undefined) {
+
+                usd_nbrb_next = data[4].Cur_OfficialRate;
+                eur_nbrb_next = data[5].Cur_OfficialRate;
+                rub_nbrb_next = data[16].Cur_OfficialRate;
+
+                console.log('Нац.банк USD завтра: ' + data[4].Cur_OfficialRate + 'дата: ' + data[4].Date + 'дата: ' + usd_nbrb_next);
+
+            }
+            flagNbrbNext = true;
+
+            showNbrb();
+
         },
         function (error) {
             // Failure!
             console.log('Failed data: ' + JSON.stringify(error));
         }
     );
-  
-//   if (flag) {
-//     console.log('Курсы за сегодня');
-//   } else {
-//     console.log('Курсы за сегодня и завтра');
-//   }
+}
+
+function showNbrb() {
+
+    if (flagNbrb == true && flagNbrbNext == true) {
+
+        if (usd_nbrb_next) {
+            loadingTitleCurrency_nbrbToday.text('СЕГОДНЯ  |  ЗАВТРА');
+            loadingValueCurrency_nbrb.text(usd_nbrb + ' $ ' + usd_nbrb_next + '\n' + eur_nbrb + ' € ' + eur_nbrb_next + ' \n ' + rub_nbrb + ' P ' + rub_nbrb_next);
+            console.log('Курсы за сегодня и завтра!');
+        } else {
+            loadingTitleCurrency_nbrbToday.text('КУРСЫ НА СЕГОДНЯ');
+            loadingValueCurrency_nbrb.text('$ ' + usd_nbrb + '\n' + ' € ' + eur_nbrb + ' \n ' + ' P ' + rub_nbrb);
+            console.log('Курсы за сегодня!');
+
+        }
+    }
+
 }
 
 // реакция на нажатие кнопки select 
 WindowWeather.on('click', 'select', updateWeather);
 WindowCurrency.on('click', 'select', updateCurrency);
-// реакция на долгое нажатие select 
-WindowWeather.on('longClick', 'select', updateWeatherHome);
+WindowCurrency.on('click', 'select', updateCurrencyNbrb);
 
-//updateWeather();
+// реакция на долгое нажатие select 
+WindowWeather.on('longClick', 'select', WeatherHomeShow);
 
 console.log('Отработано');
